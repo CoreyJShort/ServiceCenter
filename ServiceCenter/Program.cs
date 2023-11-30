b35df56a42de046f8f9f169164294b5c0bb69e0c
Corey Short <corey.short@marquette.edu>
2023-11-30 14:16:27 -0600

    Add project files.

---
+++ ServiceCenter/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ServiceCenter;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        // right click ServiceCenter, add new folder, name folder 'Model'
+        // go to Git Changes, Create repository, uncleck private
+
+
+
+    }
+}
