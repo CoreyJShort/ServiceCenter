aa2605ca0a4db920d414e84e96516a6564c57740
Corey Short <corey.short@marquette.edu>
2023-11-30 14:59:47 -0600

    Create Appointment and CustomerAppointment classes.

---
+++ ServiceCenter/Model/Appointment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Model;
+
+public class Appointment //"time slots"
+{
+    public string Id { get; set; }
+    public string Notes { get; set; } // this is not neccesary but it is nice to have
+    public DateTime dateTime { get; set; }
+
+}
