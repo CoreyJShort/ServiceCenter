50b61f3c7f619c93dff1b38c509fd3f2070c6ef5
Corey Short <corey.short@marquette.edu>
2023-11-30 14:47:29 -0600

    Create Customer Class

--- ServiceCenter/Program.cs
+++ ServiceCenter/Program.cs
@@ -5,7 +5,7 @@ public class Program
     static void Main(string[] args)
     {
         // right click ServiceCenter, add new folder, name folder 'Model'
-        // go to Git Changes, Create repository, uncleck private
+        // go to Git Changes, Create repository, uncheck private, pick a license, create & push
 
 
 
